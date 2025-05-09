32b148acd2c36bfd40e8b69b1b6a31d5a4c22b1b
Roli-Ram <82097305+Roli-Ram@users.noreply.github.com>
2025-05-09 12:40:38 +0800

    Add files via upload

--- PesticideTestAlpha/script.js
+++ PesticideTestAlpha/script.js
@@ -32,7 +32,9 @@ async function startCamera() {
         video.onloadedmetadata = () => {
             video.play();
         };
-        analyzeBtn.disabled = false;
+        document.body.style.overflow = '';  // 解鎖滾動
+            document.body.style.overflow = '';  // 解鎖滾動
+    analyzeBtn.disabled = false;
         stopBtn.disabled = true;
     } catch (err) {
         console.error("無法啟動攝像頭: ", err);
@@ -143,6 +145,7 @@ analyzeBtn.addEventListener('click', async function () {
     let intervalCount = 0;
 
     stopBtn.disabled = false;
+    document.body.style.overflow = 'hidden';  // 鎖定滾動
     analyzeBtn.disabled = true;
 
     await toggleTorch(true);
@@ -168,7 +171,9 @@ analyzeBtn.addEventListener('click', async function () {
             clearInterval(interval);
             result.innerHTML += `<h3>取樣結果 (每2秒):</h3>`;
             downloadExcel(logRGBValues);
-            analyzeBtn.disabled = false;
+            document.body.style.overflow = '';  // 解鎖滾動
+            document.body.style.overflow = '';  // 解鎖滾動
+    analyzeBtn.disabled = false;
             stopBtn.disabled = true;
             toggleTorch(false);
         }
@@ -179,9 +184,12 @@ analyzeBtn.addEventListener('click', async function () {
 });
 
 stopBtn.addEventListener('click', function () {
+    document.body.style.overflow = '';  // 解鎖滾動
     clearInterval(interval);
     result.innerHTML += `<h3>取樣已提前結束</h3>`;
     downloadExcel(logRGBValues);
+    document.body.style.overflow = '';  // 解鎖滾動
+            document.body.style.overflow = '';  // 解鎖滾動
     analyzeBtn.disabled = false;
     stopBtn.disabled = true;
     toggleTorch(false);
